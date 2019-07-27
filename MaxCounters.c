1901f2e41cb4efe8c4cda3cc08c76dd79fd9e494
korkland <52877596+korkland@users.noreply.github.com>
2019-07-27 11:40:39 +0300

    Update MaxCounters.c

--- MaxCounters.c
+++ MaxCounters.c
@@ -1,12 +1,13 @@
-void init_max(int *ptr,int size,int max);
-int find_max(int *ptr,int size);
+void init_max(int *ptr,int size, int set_to_max);
+
 struct Results solution(int N, int A[], int M) {
     int max=0,set_to_max=0;
     struct Results result;
     // write your code in C99 (gcc 6.2.0)
-    result.C = (int *)calloc(N,sizeof(int));
+    result.C = (int *)calloc(N,sizeof(int));//initial new arr with zeros
+    
     for(int i=0; i<M;i++){
-        if(A[i]<=N){
+        if(A[i]<=N){//increase element by 1 and update the max value
             if(result.C[A[i]-1]<set_to_max)
                 result.C[A[i]-1]=set_to_max;
             result.C[A[i]-1]+=1;
@@ -14,9 +15,10 @@ struct Results solution(int N, int A[], int M) {
                 max=result.C[A[i]-1];
         }
         else{
-            set_to_max=max;
+            set_to_max=max; //update the set value
         }
     }
+    // at the end set elements that lesser than set_to_max to set_to_max
     init_max(result.C,N,set_to_max);
     result.L = N;
     return result;
